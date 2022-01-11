12726f08415f4910679afb02bf139afb6152ebd7
Michael Savin <jtprogru@gmail.com>
2022-01-11 21:48:22 +0300

    Update scripts and dependecies
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

--- scripts/start.js
+++ scripts/start.js
@@ -5,18 +5,18 @@ const browserSyncPath = upath.resolve(upath.dirname(__filename), '../node_module
 
 concurrently([
     { command: 'node scripts/sb-watch.js', name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
-    { 
+    {
         command: `"${browserSyncPath}" --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
-        name: 'SB_BROWSER_SYNC', 
+        name: 'SB_BROWSER_SYNC',
         prefixColor: 'bgGreen.bold',
     }
 ], {
     prefix: 'name',
     killOthers: ['failure', 'success'],
-}).then(success, failure);
+}).result.then(success, failure);
 
 function success() {
-    console.log('Success');    
+    console.log('Success');
 }
 
 function failure() {
